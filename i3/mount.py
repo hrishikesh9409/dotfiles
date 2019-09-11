9e144b8297d152d9d516e16d239797200e17fc34
hrishikesh9409 <hrishikesh9409@gmail.com>
2019-09-11 20:11:23 +0530

    dummy commit

--- i3/mount.py
+++ i3/mount.py
@@ -1,16 +1,19 @@
 import os
+import getpass 
+
+username = getpass.getuser()
 
 os.system("sudo fdisk -l")
 drive_to_be_mounted=raw_input("enter drive to be mounted: ")
 
 dir_no=0
 for i in range(0, 10):
-    if(os.path.isdir("/home/hrishi/mount"+str(i))):
+    if(os.path.isdir("/home/"+username+"/mount"+str(i))):
         pass
     else:
-        os.mkdir("/home/hrishi/mount"+str(i))
+        os.mkdir("/home/"+username+"/mount"+str(i))
         dir_no=i
         break
 
-print("sudo mount /dev/"+drive_to_be_mounted+" $HOME/mount"+str(dir_no))
-os.system("sudo mount /dev/"+drive_to_be_mounted+" $HOME/mount"+str(dir_no))
+print("sudo mount /dev/"+drive_to_be_mounted+" /home/"+username+"/mount"+str(dir_no))
+os.system("sudo mount /dev/"+drive_to_be_mounted+" /home/"+username+"/mount"+str(dir_no))
--- i3/umount.py
+++ i3/umount.py
@@ -1,6 +1,9 @@
 import os
+import getpass 
+
+username = getpass.getuser()
 
 os.system("ls | grep mount")
 drive_to_be_umounted = input("mount# to be umounted (enter dir #): ")
 os.system("sudo umount mount"+str(drive_to_be_umounted))
-os.rmdir("/home/hrishi/mount"+str(drive_to_be_umounted))
+os.rmdir("/home/"+username+"/mount"+str(drive_to_be_umounted))
